dc098c6cbeb1ba32ddc72333f38d53a96d71cde6
Nishant Garg <nishgarg14@gmail.com>
2014-12-02 12:48:29 -0600

    Final changes to Search Engine

---
+++ Search-Engine-for-specific-collection/Readme.txt
@@ -0,0 +1,21 @@
+Execution Instructions
+----------------------
+
+python rank.py pathToQueryFile pathToOutputFile
+
+
+Note
+----
+
+Use Query file present with this project as the query files provided
+by professor is not working because of some encoding issues.
+
+Index_Version2 is used for index information.
+
+stopwords file is also included in the project.
+
+
+Dependencies
+------------
+python 2.7
+NLKT/ NLTK data
--- Search-Engine-for-specific-collection/rank.py
+++ Search-Engine-for-specific-collection/rank.py
@@ -11,30 +11,69 @@ import time
 from query_parse import Query
 from index_load import get_collectionsize, get_avgdoclen, headline
 
+def rank(query):
+
+    q = Query(query)
+    output = []
+    try:
+        output.append("Indexed form for query : %s" % q.indexedForm)
+        output.append("As per W1 weight function :\n--------------------------")
+        output.append("     Rank Doc ID   Score   Headline")
+        i = 0
+        for docid,score in  q.ranked_w1[:10]:
+            i += 1
+            output.append("     %-6i%-8i%-.5f  %-s" % (i, docid, score, headline(docid)))
+        output.append("As per W2 weight function :\n--------------------------")
+        output.append("     Rank Doc ID   Score   Headline")
+        i = 0
+        for docid,score in  q.ranked_w2[:10]:
+            i += 1
+            output.append("     %-6i%-8i%-.5f  %-s" % (i, docid, score, headline(docid)))
+    except Exception as e:
+        print e
+        print 'Query is not in acceptable format. Please try again. %s ' % query
+        sys.exit()
+
+    return output
+
+
+
 if __name__ == '__main__':
+
+
+    if len(sys.argv) != 3:
+        print "Please use this format. python rank.py pathToQueryFile pathToOutputFile"
+
+    query_file = sys.argv[1]
+    output_file = sys.argv[2]
 
     print "Welcome to document ranking system"
     print "Collection Size : %s\nAverage Doc length: %s \nPlease enter the query -" \
              % (str(get_collectionsize()), str(get_avgdoclen()))
-    while(1):
+    
+    q = open(query_file,'r')
+    f = open(output_file,'w')
+
+    print "Reading Query file : %s" % query_file
+    queries = [str(line.strip("\n").strip()) for line in q.readlines() if len(line) > 3]
+    q.close()
+    print "Total Queries %s" % len(queries)
+
+    print "Starting processing queries..."
+    i = 0
+    for query in queries:
+        i += 1
+        print "Processing query %d  : %s" % (i,query)
+        f.write("Query "+ str(i) +" : " + query + "\n")
+        output = rank(query)
+        for line in output:
+            f.write(line+"\n")
+        f.write("\n\n")
+    f.close()
+
+    # For command line Interface for query input. Set to false
+    while(False):
         query = raw_input('>>')
         if query == 'exit':
             break
-        q = Query(query)
-        try:
-            print "Indexed form for query : %s" % q.indexedForm
-            print "As per W1 weight function :\n--------------------------"
-            print "     Rank Doc ID   Score   Headline"
-            i = 0
-            for docid,score in  q.ranked_w1[:10]:
-                i += 1
-                print "     %-6i%-8i%-.5f  %-s" % (i, docid, score, headline(docid))
-            print "As per W2 weight function :\n--------------------------"
-            print "     Rank Doc ID   Score   Headline"
-            i = 0
-            for docid,score in  q.ranked_w2[:10]:
-                i += 1
-                print "     %-6i%-8i%-.5f  %-s" % (i, docid, score, headline(docid))
-        except Exception as e:
-            print e
-            print 'Query is not in acceptable format. Please try again.'
+        rank(query)
